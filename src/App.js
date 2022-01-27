688bd17a005e80cef96a4db2a9c379e48ac82702
Xena123kiki <ckdnwls1215@snu.ac.kr>
2022-01-27 15:02:15 +0900

    todo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo'
 
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Todo/>
     </div>
   );
 }
